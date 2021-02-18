594bc3f3391c67c49c0c883a1fc9cf4788379e1f
sang hun <pooij96@naver.com>
2021-02-19 01:07:09 +0900

    6. Button-ex1

---
+++ Button-ex1/app/src/main/java/com/cookandroid/android1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.cookandroid.android1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.DialogTitle;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity2";
+    private Button btnAdd, btnMinus;
+    private TextView tvNum;
+    private Integer num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        initSetting();
+        initListener();
+    }
+
+    private void init(){
+       btnAdd = findViewById(R.id.btn_add);
+       btnMinus = findViewById(R.id.btn_minus);
+       tvNum = findViewById(R.id.tv_num);
+    }
+
+    private void initSetting(){
+        num = 1;
+        tvNum.setText(num.toString());
+    }
+
+    private void initListener(){
+        btnAdd.setOnClickListener(v -> {
+            Log.d(TAG, "initListener: ");
+            num = num + 1;
+            tvNum.setText(num.toString());
+        });
+
+        btnMinus.setOnClickListener(v -> {
+            Log.d(TAG, "initListener: ");
+            num = num - 1;
+            tvNum.setText(num.toString());
+        });
+    }
+}
